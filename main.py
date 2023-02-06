8914c1503d276ec51f7144453463deaa5e1fddfe
Jasper Landa <jasperlanda91@gmail.com>
2023-02-06 15:53:43 +0100

    Separated calculation of answer and csv builder

--- main.py
+++ main.py
@@ -11,37 +11,40 @@ def all_factors(kingdom_combination, kingdom_factors, non_combination, non_facto
     return kingdom_factors, non_factors, joint_factors(*kingdom_combination, *non_combination)
 
 
-def main(output_csv=False):
+def output_csv():
+    t1 = perf_counter_ns()
+    joint_gen = product(CONFIG.generators.kingdom_cards(), CONFIG.generators.non_cards())
+    kingdom_gen_with_factors = IdentityWithMap(CONFIG.generators.kingdom_cards(), kingdom_cards_factors)
+    non_gen_with_factors = IdentityWithMap(CONFIG.generators.non_cards(), non_cards_factors)
+    all_factors_gen = (pair_product(all_factors(kingdom_combi, kingdom_factors, non_combi, non_factors)) for
+                       (kingdom_combi, kingdom_factors), (non_combi, non_factors) in
+                       product(kingdom_gen_with_factors, non_gen_with_factors))
+    with open('combination_values.csv', 'w') as f:
+        f.write(f"{','.join(tuple(CONFIG.generators.kingdom_cards.card_totals.keys()) + tuple(CONFIG.generators.non_cards.card_totals.keys()))}\n")
+        for (kingdom_combi, non_combi), factors in zip(joint_gen, all_factors_gen):
+            f.write(f"{','.join(map(str, kingdom_combi + non_combi + factors))}\n")
+    t2 = perf_counter_ns()
+    print("Written entire csv")
+    print()
+    print(f"Elapsed time: {(t2 - t1) / 1e9} s")
 
-    if output_csv:
-        t1 = perf_counter_ns()
-        joint_gen = product(CONFIG.generators.kingdom_cards(), CONFIG.generators.non_cards())
-        kingdom_gen_with_factors = IdentityWithMap(CONFIG.generators.kingdom_cards(), kingdom_cards_factors)
-        non_gen_with_factors = IdentityWithMap(CONFIG.generators.non_cards(), non_cards_factors)
-        all_factors_gen = (pair_product(all_factors(kingdom_combi, kingdom_factors, non_combi, non_factors)) for (kingdom_combi, kingdom_factors), (non_combi, non_factors) in product(kingdom_gen_with_factors, non_gen_with_factors))
-        with open('combination_values.csv', 'w') as f:
-            f.write(f"{','.join(tuple(CONFIG.generators.kingdom_cards.card_totals.keys()) + tuple(CONFIG.generators.non_cards.card_totals.keys()))}\n")
-            for (kingdom_combi, non_combi), factors in zip(joint_gen, all_factors_gen):
-                f.write(f"{','.join(map(str, kingdom_combi + non_combi + factors))}\n")
-        t2 = perf_counter_ns()
-        print("Written entire csv")
-        print()
-        print(f"Elapsed time: {(t2 - t1) / 1e9} s")
 
-    else:
-        t1 = perf_counter_ns()
-        kingdom_gen_with_factors = IdentityWithMap(CONFIG.generators.kingdom_cards(), kingdom_cards_factors)
-        non_gen_with_factors = IdentityWithMap(CONFIG.generators.non_cards(), non_cards_factors)
-        all_factors_gen = (pair_product(all_factors(kingdom_combi, kingdom_factors, non_combi, non_factors)) for (kingdom_combi, kingdom_factors), (non_combi, non_factors) in product(kingdom_gen_with_factors, non_gen_with_factors))
-        total_unordered, total_ordered = pair_sum(all_factors_gen)
-        t2 = perf_counter_ns()
+def main():
+    t1 = perf_counter_ns()
+    kingdom_gen_with_factors = IdentityWithMap(CONFIG.generators.kingdom_cards(), kingdom_cards_factors)
+    non_gen_with_factors = IdentityWithMap(CONFIG.generators.non_cards(), non_cards_factors)
+    all_factors_gen = (pair_product(all_factors(kingdom_combi, kingdom_factors, non_combi, non_factors)) for
+                       (kingdom_combi, kingdom_factors), (non_combi, non_factors) in
+                       product(kingdom_gen_with_factors, non_gen_with_factors))
+    total_unordered, total_ordered = pair_sum(all_factors_gen)
+    t2 = perf_counter_ns()
 
-        print(f"Total unique kingdoms:")
-        print(f"Not considering deck orderings:\t{total_unordered}")
-        print(f"Considering deck orderings:\t\t{total_ordered}")
-        print()
-        print(f"Elapsed time: {(t2 - t1) / 1e9} s")
+    print(f"Total unique kingdoms:")
+    print(f"Not considering deck orderings:\t{total_unordered}")
+    print(f"Considering deck orderings:\t\t{total_ordered}")
+    print()
+    print(f"Elapsed time: {(t2 - t1) / 1e9} s")
 
 
 if __name__ == '__main__':
-    main(output_csv=True)
+    main()
