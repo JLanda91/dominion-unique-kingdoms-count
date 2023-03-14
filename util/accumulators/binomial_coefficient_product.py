ea02d6653dbc79b67a88d5674039edc3be7a9df0
Jasper Landa <jasperlanda91@gmail.com>
2023-03-14 20:20:39 +0100

    assert on n and k iterables

--- util/accumulators/binomial_coefficient_product.py
+++ util/accumulators/binomial_coefficient_product.py
@@ -5,5 +5,6 @@ from math import comb
 
 
 def binomial_coefficient_product(n_collection, k_collection):
+    assert len(n_collection) == len(k_collection), f"Unequal amount of n and k coefficients ({len(n_collection)} and {len(k_collection)} respectively)"
     return reduce(mul, starmap(comb, zip(n_collection, k_collection)), 1)
 
